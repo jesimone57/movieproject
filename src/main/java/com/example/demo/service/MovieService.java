79a6479800090faba6d38a37598455f499b05cf4
jsimone <jsimone@aip.com>
2025-11-23 15:20:48 -0500

    imports

--- src/main/java/com/example/demo/service/MovieService.java
+++ src/main/java/com/example/demo/service/MovieService.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
